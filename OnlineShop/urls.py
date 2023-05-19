1b761f9e2ded0c81c3b85d78bb1890974825e814
aurkhan999 <aurkhan999@gmail.com>
2023-05-19 12:37:07 +0500

    first commit

---
+++ OnlineShop/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+# from core.views import index, contact
+
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
+    # path('contact/', contact, name='contact'),
+    path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
